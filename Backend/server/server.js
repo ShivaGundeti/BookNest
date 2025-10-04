8a9ae8c41dd46c348eee05fa4c9198292b3befea
shiva Kumar <shivagundeti1403@gmail.com>
2025-10-04 23:24:06 +0530

    Configure CORS middleware to specify allowed origins and methods

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -7,7 +7,13 @@ import cors from "cors"
 import { ConnectMongodb } from "../lib/mongo/db.js"
 const app = express()
 dotenv.config()
-app.use(cors())
+app.use(
+  cors({
+    origin: ["https://book-nest-beryl-alpha.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json())
 app.use("/auth",auth)
 app.use("/books",Book)
